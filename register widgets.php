3fb06edffaf8f5d13c969b0552f0bc7dd337159b
Mutalqureshi <35372795+Mutalqureshi@users.noreply.github.com>
2019-09-25 14:39:54 +0500

    Add files via upload

---
+++ register widgets.php
@@ -0,0 +1,34 @@
+<?php 
+function naked_register_sidebars() {
+	register_sidebar(array(					// Start a series of sidebars to register
+		'id' => 'sidebar', 					// Make an ID
+		'name' => 'Sidebar',				// Name it
+		'description' => 'Take it on the side...', // Dumb description for the admin side
+		'before_widget' => '<div class="widget">',	// What to display before each widget
+		'after_widget' => '</div>',	// What to display following each widget
+		'before_title' => '<span class="title-side-bar">',	// What to display before each widget's title
+		'after_title' => '</span>',		
+		'empty_title'=> '',					// What to display in the case of no title defined for a widget
+	));
+} 
+add_action( 'widgets_init', 'naked_register_sidebars' );
+/*-----------------------------------------------------------
+*/
+
+ 
+function addnewwidget(){
+
+ register_sidebar(
+      array(
+        'name'          => 'Blog Sidebar',
+        'id'            => 'blog-sidebar',    // ID should be LOWERCASE  ! ! !
+        'before_widget' => '<div class="side-bar">',
+        'after_widget'  => '</div>',
+        'before_title'  => '',
+        'after_title'   => '' )
+    );
+
+}
+
+add_action('widgets_init', 'addnewwidget');
+ ?>
