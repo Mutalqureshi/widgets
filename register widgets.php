4866645090c93706cc95e068b8a5b1d823afdf16
Mutalqureshi <35372795+Mutalqureshi@users.noreply.github.com>
2019-10-14 14:17:33 +0500

    Add files via upload

--- register widgets.php
+++ register widgets.php
@@ -32,3 +32,9 @@ function addnewwidget(){
 
 add_action('widgets_init', 'addnewwidget');
  ?>
+
+<?php if ( is_active_sidebar( 'left-sidebar' ) ) : ?>
+	<ul id="sidebar">
+		<?php dynamic_sidebar( 'left-sidebar' ); ?>
+	</ul>
+<?php endif; ?>
